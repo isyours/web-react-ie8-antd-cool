37a52f1706d459dd7ceb8d2d89e7cdf6e111a3cd
chlde <411150056@qq.com>
2017-09-07 23:32:31 +0800

    update antd

--- src/page/index.js
+++ src/page/index.js
@@ -3,15 +3,12 @@ require('es5-shim/es5-sham');
 require('console-polyfill');
 require('fetch-ie8');
 
+
+import { Layout, Menu, Breadcrumb  } from 'antd';
+import 'antd/dist/antd.css';
+const { Header, Content, Footer } = Layout;
 const React = require('react');
 const ReactDOM = require('react-dom');
-
-import HeaderComp from '../components/header'
-
-//antd全家桶的css
-import { Slider } from 'antd';
-import DatePicker from 'antd/lib/date-picker';
-import 'antd/dist/antd.css';
 
 
 function onChange(checked) {
@@ -22,13 +19,31 @@ function onChange(checked) {
 class App extends React.Component{
     render(){
         return(
-            <div>
-                <HeaderComp></HeaderComp>
-                <Slider defaultValue={30} />
-                <DatePicker/>
-                <h1>欢迎来到首页!!!</h1>
-                <a href="./user.html">去用户页面</a>
-            </div>
+            <Layout>
+                <Header style={{ position: 'fixed', width: '100%' }}>
+                    <div className="logo" />
+                    <Menu
+                        theme="dark"
+                        mode="horizontal"
+                        defaultSelectedKeys={['1']}
+                        style={{ lineHeight: '64px' }}
+                    >
+                        <Menu.Item key="1">首页</Menu.Item>
+                        <Menu.Item key="2">公司简介</Menu.Item>
+                        <Menu.Item key="3">工程项目</Menu.Item>
+                        <Menu.Item key="4">联系我们</Menu.Item>
+                    </Menu>
+                </Header>
+                <Content style={{ padding: '0 50px', marginTop: 64 }}>
+                    <Breadcrumb style={{ margin: '12px 0' }}>
+                        <Breadcrumb.Item>首页</Breadcrumb.Item>
+                    </Breadcrumb>
+                    <div style={{ background: '#fff', padding: 24, minHeight: 380 }}>Content</div>
+                </Content>
+                <Footer style={{ textAlign: 'center' }}>
+                    Ant Design ©2016 Created by Ant UED
+                </Footer>
+            </Layout>
         )
     }
 }
